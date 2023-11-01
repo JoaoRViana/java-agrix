2fc3604a59a569a38bed740f3d9bc6df888d7831
JoaoRViana <joaorviana98@gmail.com>
2023-11-01 15:41:00 -0300

    req01?

---
+++ src/main/java/com/betrybe/agrix/controllers/PersonController.java
@@ -0,0 +1,13 @@
+package com.betrybe.agrix.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Crop.
+ */
+@RestController
+@RequestMapping(value = "/persons")
+public class PersonController {
+    
+}
